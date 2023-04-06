f71c89bfbbd273df6b537524003cbcb6b5c87993
Filip <filiptarajko@gmail.com>
2023-04-06 21:36:38 +0200

    add keymap visualization

---
+++ maps.c
@@ -0,0 +1,64 @@
+KEY NUMBERS
+// |    0    |    1    |    2    |    3    |     4   |        |     5   |     6   |    7    |    8    |    9    |
+// |   10    |   11    |   12    |   13    |    14   |        |    15   |    16   |   17    |   18    |   19    |
+// |   20    |   21    |   22    |   23    |    24   |        |    25   |    26   |   27    |   28    |   29    |
+//                               |   30    |    31   |        |    32   |    33   |
+
+COMBO KEYS
+#define COMBO_ESC 3 4
+#define COMBO_TAB 13 14
+#define COMBO_NAV 30 31
+#define COMBO_CTR 32 33
+#define COMBO_ANKI 26 27 28 29
+#define COMBO_BT_CLR 0 10 20 9 19 29
+#define COMBO_BT_PRV 0 10 20 15
+#define COMBO_BT_NXT 0 10 20 18
+
+default_layer
+// |    Q    |    W    |    E    |    R    |    T    |        |    Y    |    U    |    I    |    O    |    P    |
+// |    A    |    S    |    D    |    F    |    G    |        |    H    |    J    |    K    |    L    |    ;    |
+// |    Z    |    X    |    C    |    V    |    B    |        |    N    |    M    |    ,    |    .    |    /    |
+//                               |   SYM   |   SPC   |        |  ENTER  |   NUM   |
+
+number_layer
+// |         |    7    |    8    |    9    |    /    |        |         |    +    |    -    |    *    |    \    |
+// |    0    |    4    |    5    |    6    |    =    |        |  BSPC   | NUM_SYM |    =    |   DEL   |    ;    |
+// |         |    1    |    2    |    3    |    -    |        |         |    _    |    ,    |    .    |    /    |
+//                               |   FUN   |   SPC   |        |  ENTER  | PRESSED |
+
+number_symbol_layer
+// |         |    &    |    *    |    (    |    ?    |        |         |    +    |    -    |    *    |    \    |
+// |    )    |    $    |    %    |    ^    |    +    |        |  BSPC   | PRESSED |    =    |   DEL   |    ;    |
+// |         |    !    |    @    |    #    |    _    |        |         |    _    |    ,    |    .    |    /    |
+//                               |   FUN   |   SPC   |        |  ENTER  | PRESSED |
+
+symbol_layer
+// |         |         |         |         |         |        |   ` ~   |    @    |    &    |    |    |    \    |
+// |         |         |         |         |         |        |   " '   |   ( )   |   { }   |   [ ]   |   < >   |
+// |         |         |         |         |         |        |    $    |    %    |    !    |    #    |    ^    |
+//                               | PRESSED |   SPC   |        |  ENTER  |   FUN   | 
+
+navigation_layer
+// |         |         |         |         |         |        |   Redo  |  Paste  |  Copy   |   Cut   |  Undo   |
+// |         |         |         |         |         |        |    ←    |    ↓    |    ↑    |    →    |  Caps   |
+// |         |         |         |         |         |        |   Home  | Pg down |  Pg up  |   End   |  Insert |
+//                               |  COMBO  |  COMBO  |        |         |         |
+
+control_layer
+// Everything is gui modified
+// |         |         |         |         |         |        |         |          |        |         |         |
+// |    0    |    4    |  lCTRL  |  SHIFT  |         |        |    ←    |     ↓    |    ↑   |    →    |         |
+// |         |    1    |    2    |    3    |         |        |         |          |        |         |         |
+//                               |         |         |        |  COMBO  |   COMBO  |
+
+function_layer
+// |   F12   |    F7   |    F8   |    F9   |  PrtScn |        |   C_RW  |  vol 1/0 |  K_APP |  C_FF   |         |
+// |   F11   |    F4   |    F5   |    F6   |         |        | med prev|   vol-   |   vol+ | med next|         |
+// |   F10   |    F1   |    F2   |    F3   |         |        |         | media p/p|        |         |         |
+//                               | PRESSED |         |        |         |  PRESSED |
+
+anki_layer
+// |&tog ANKI|         |    T    |    Y    |  K_REDO |        | K_REDO  |    Y     |    T   |         |         |
+// |         |    1    |    2    |  SPACE  |    4    |        |    4    |  SPACE   |    2   |    1    |         |
+// |         |         |    B    |    S    |  K_UNDO |        | K_UNDO  |    S     |    B   |         |         |
+//                               |  SPACE  |         |        |         |  SPACE   |
